798d874375bdbd2b3fd226b824e9b0e63dc8e63a
Vaqueiro7 <106885404+Vaqueiro7@users.noreply.github.com>
2022-06-06 21:13:57 -0300

    Update MergeSort.java

--- OrdenaçaoInterna/src/all/MergeSort.java
+++ OrdenaçaoInterna/src/all/MergeSort.java
@@ -10,48 +10,52 @@
 /*************************************************************/
 package all;
 
-
 public class MergeSort {
-    
-        //atributos do algoritmo Merge Sort
-        private long tempo;
+
+	// atributos do algoritmo Merge Sort
+	private long tempo;
 	private int contadorAM;
 	private int compM;
-        private int[] ordenado;
+	private int[] ordenado;
 
 	public MergeSort(int[] vetor, int n) {
-            
-                //Método que irá ordenar os vetor com o algoritmo Merge Sort e calculará o tempo gasto em milisegundos
-                System.out.println("\nOrdenação com o Merge Sort: " + n + " valores.");
-                this.ordenado = CriaVetor.copiarVetor(vetor, n);
-		this.tempo = System.nanoTime();
-		ordenaVetorMergeSort(ordenado, n );
-		this.tempo = System.nanoTime() - this.tempo;
-        }
 
-        //Função para calcular o tempo gasto para a ordenação
+		// Método que irá ordenar os vetor com o algoritmo Merge Sort e calculará o
+		// tempo gasto em milisegundos
+		System.out.println("\nOrdenação com o Merge Sort: " + n + " valores.");
+		this.ordenado = CriaVetor.copiarVetor(vetor, n);
+		this.tempo = System.nanoTime();
+		ordenaVetorMergeSort(ordenado, n);
+		this.tempo = System.nanoTime() - this.tempo;
+	}
+
+	// Função para calcular o tempo gasto para a ordenação
 	public float gettempo() {
-            return this.tempo / (float) 1000000;
+		return this.tempo / (float) 1000000;
 	}
-        //Função para calcular o número de acessos do Merge Sort
+
+	// Função para calcular o número de acessos do Merge Sort
 	public int getcontadorA() {
-    	return this.contadorAM;
+		return this.contadorAM;
 	}
-        //Função para calcular o número de Comparações do Merge Sort
+
+	// Função para calcular o número de Comparações do Merge Sort
 	public int getcompM() {
-            return this.compM;
+		return this.compM;
 	}
-        //Função para pegar o vetor "ordenado", que será o vetor original ordenado
-        public int[] getOrdenado(){
-            return this.ordenado;
-        }
-        //desenvolvimento do algoritmo (ordenação dos 'n' primeiros valores do vetor)
+
+	// Função para pegar o vetor "ordenado", que será o vetor original ordenado
+	public int[] getOrdenado() {
+		return this.ordenado;
+	}
+
+	// desenvolvimento do algoritmo (ordenação dos 'n' primeiros valores do vetor)
 	public void ordenaVetorMergeSort(int[] vetor, int n) {
 
 		if (n < 2) {
 			this.compM++;
 			return;
-                        }
+		}
 		int meio = n / 2;
 		int[] esquerda = new int[meio];
 		int[] direita = new int[n - meio];
@@ -68,7 +72,8 @@ public class MergeSort {
 		merge(vetor, esquerda, direita, meio, n - meio);
 	}
 
-        //função que realiza as ordenações de forma recursiva e calcula o número de comparações e acessos
+	// função que realiza as ordenações de forma recursiva e calcula o número de
+	// comparações e acessos
 	public void merge(int[] vetor, int[] esquerda, int[] direita, int left, int right) {
 
 		int i = 0, j = 0, k = 0;
